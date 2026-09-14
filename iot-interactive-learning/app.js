0dff8d84ba599cf99180d112ec30a8fa22a20846
phuwishpk <phuwish123@gmail.com>
2026-09-15 03:23:52 +0700

    fix express routing issue

--- iot-interactive-learning/app.js
+++ iot-interactive-learning/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA Fallback: ให้ React Router จัดการ Path แทน (กรณีคนกด Refresh หน้าเว็บ)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
